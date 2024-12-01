0b834a1cb0a66d38452f99c18399b0df8da1a4ad
Zandile Masilo <zandimasilo@gmail.com>
2024-12-01 22:53:01 +0000

    Add Post and Category models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+   
+def __str__(self): 
+  return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField(default='Default Content')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices= STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    
+
+class META:
+    ordering = ["created_on"] 
+
+
+def __str__(self):
+  return f"{self.title} | written by {self.author}"
